cfe8c069f0fa39e6a4e8afe36ce7fc67841cfe41
Allan Ritchie <allan.ritchie@gmail.com>
2026-02-12 21:41:58 -0500

    Fix nav

--- samples/Sample/App.cs
+++ samples/Sample/App.cs
@@ -8,10 +8,9 @@ public class App : Microsoft.Maui.Controls.Application
     protected override Window CreateWindow(IActivationState? activationState)
     {
         var tabbedPage = new TabbedPage();
-        tabbedPage.Children.Add(new MainPage { Title = "Controls" });
-        tabbedPage.Children.Add(new CollectionViewPage { Title = "Collection" });
-        tabbedPage.Children.Add(new EssentialsPage { Title = "Essentials" });
-        tabbedPage.Children.Add(new StoragePage { Title = "Storage" });
+        tabbedPage.Children.Add(new NavigationPage(new MainPage { Title = "Controls" }) { Title = "Controls" });
+        tabbedPage.Children.Add(new NavigationPage(new EssentialsPage { Title = "Essentials" }) { Title = "Essentials" });
+        tabbedPage.Children.Add(new NavigationPage(new StoragePage { Title = "Storage" }) { Title = "Storage" });
         return new Window(tabbedPage);
     }
 }
